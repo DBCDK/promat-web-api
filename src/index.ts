4f0d0719689916f3a9eca147895000cb71fc75dc
Esben von Buchwald <esben@vonbuchwald.dk>
2020-10-06 15:57:10 +0200

    Enable express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world")
+import * as express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send({ message: "hello world" });
+});
+
+app.listen(port, () => {
+  console.log("Server has been started");
+});
